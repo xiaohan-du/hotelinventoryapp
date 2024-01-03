0d8b7b57f53a7a7221e0476952d99c6f5248fa7d
thinkvantagedu <duxiaohan2006@gmail.com>
2024-01-03 11:52:39 +0000

    Add and use a new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from "./rooms/rooms.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, RoomsComponent]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  standalone: true,
+  imports: [],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.scss'
+})
+export class RoomsComponent implements OnInit {
+  hotelName = 'Hilton Hotel';
+  constructor() {}
+  ngOnInit(): void {
+      
+  }
+}
