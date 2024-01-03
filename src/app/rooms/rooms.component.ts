0466b4110dc4577b1ef2521a4e6fd6faba69de59
thinkvantagedu <duxiaohan2006@gmail.com>
2024-01-03 12:11:46 +0000

    Conditionally show room number

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -11,7 +11,6 @@ import { CommonModule } from '@angular/common';
 })
 export class RoomsComponent implements OnInit {
   hotelName = 'Hilton Hotel';
-  noOfRooms = 10;
   hideRooms = false;
 
   rooms: Room = {
