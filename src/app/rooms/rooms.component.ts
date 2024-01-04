6037edbc661af3379d1113d4000d659eeaf968e7
thinkvantagedu <duxiaohan2006@gmail.com>
2024-01-04 10:37:28 +0000

    Pipe: number

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -27,7 +27,8 @@ export class RoomsComponent implements OnInit {
       price: 1000,
       photos: "https://plus.unsplash.com/premium_photo-1670360414903-19e5832f8bc4?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       checkinTime: new Date('01-01-2021').toLocaleString(),
-      checkoutTime: new Date('02-01-2021').toLocaleString()
+      checkoutTime: new Date('02-01-2021').toLocaleString(),
+      rating: 4.5
     },
     {
       roomNumber: 2,
@@ -36,7 +37,8 @@ export class RoomsComponent implements OnInit {
       price: 1000,
       photos: "https://images.unsplash.com/photo-1611892440504-42a792e24d32?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       checkinTime: new Date('02-01-2021').toLocaleString(),
-      checkoutTime: new Date('03-01-2021').toLocaleString()
+      checkoutTime: new Date('03-01-2021').toLocaleString(),
+      rating: 4.3
     },
     {
       roomNumber: 3,
@@ -45,7 +47,8 @@ export class RoomsComponent implements OnInit {
       price: 5000,
       photos: "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       checkinTime: new Date('03-01-2021').toLocaleString(),
-      checkoutTime: new Date('04-01-2021').toLocaleString()
+      checkoutTime: new Date('04-01-2021').toLocaleString(),
+      rating: 4.2
     },
     {
       roomNumber: 4,
@@ -54,7 +57,8 @@ export class RoomsComponent implements OnInit {
       price: 15000,
       photos: "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       checkinTime: new Date('04-01-2021').toLocaleString(),
-      checkoutTime: new Date('05-01-2021').toLocaleString()
+      checkoutTime: new Date('05-01-2021').toLocaleString(),
+      rating: 4.1
     }
   ]
 
--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -12,4 +12,5 @@ export interface RoomList {
   photos: string;
   checkinTime: string;
   checkoutTime: string;
+  rating: number;
 }
