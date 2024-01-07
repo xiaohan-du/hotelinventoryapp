f8d0ad094a25ede34afa66d9efb6310c1f460d6a
thinkvantagedu <duxiaohan2006@gmail.com>
2024-01-07 11:24:28 +0000

    ngAfterContentInit: set component property after content is initialized in ng-content

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, OnInit, ViewChild } from '@angular/core';
+import { EmployeeComponent } from '../employee/employee.component';
 
 @Component({
   selector: 'app-container',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
-export class ContainerComponent {
+export class ContainerComponent implements AfterContentInit {
+  @ContentChild(EmployeeComponent) employee!: EmployeeComponent;
 
+  ngAfterContentInit(): void {
+      this.employee.employeeName = "Nick";
+  }
 }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './employee.component.scss'
 })
 export class EmployeeComponent {
-
+  employeeName: string = 'John';
 }
