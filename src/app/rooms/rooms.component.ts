6d90157d59d52460fd381575bd78cd6439a1234f
thinkvantagedu <duxiaohan2006@gmail.com>
2024-01-09 10:54:12 +0000

    Use service to store business logic, inject service dependency into component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,6 +3,7 @@ import { Room, RoomList } from './rooms';
 import { CommonModule } from '@angular/common';
 import { RoomsListComponent } from "../rooms-list/rooms-list.component";
 import { HeaderComponent } from "../header/header.component";
+import { RoomsService } from './services/rooms.service';
 
 @Component({
     selector: 'app-rooms',
@@ -31,6 +32,7 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
   @ViewChild(HeaderComponent) headerComponent!: HeaderComponent;
 
   @ViewChildren(HeaderComponent) headerComponents!: QueryList<HeaderComponent>;
+  constructor(private roomsService: RoomsService) { }
 
   addRoom() {
     const room: RoomList = {
@@ -54,50 +56,8 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
   selectRoom(room: RoomList) {
     this.selectedRoom = room;
   }
-  constructor() {}
   ngOnInit(): void {
-    this.roomList = [
-      {
-        roomNumber: 1,
-        roomType: "Deluxe",
-        amenities: "Air Conditioner",
-        price: 1000,
-        photos: "https://plus.unsplash.com/premium_photo-1670360414903-19e5832f8bc4?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        checkinTime: new Date('01-01-2021').toLocaleString(),
-        checkoutTime: new Date('02-01-2021').toLocaleString(),
-        rating: 4.5
-      },
-      {
-        roomNumber: 2,
-        roomType: "Deluxe",
-        amenities: "Air Conditioner",
-        price: 1000,
-        photos: "https://images.unsplash.com/photo-1611892440504-42a792e24d32?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        checkinTime: new Date('02-01-2021').toLocaleString(),
-        checkoutTime: new Date('03-01-2021').toLocaleString(),
-        rating: 4.3
-      },
-      {
-        roomNumber: 3,
-        roomType: "Luxury",
-        amenities: "Air Conditioner",
-        price: 5000,
-        photos: "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        checkinTime: new Date('03-01-2021').toLocaleString(),
-        checkoutTime: new Date('04-01-2021').toLocaleString(),
-        rating: 4.2
-      },
-      {
-        roomNumber: 4,
-        roomType: "Super Luxury",
-        amenities: "Air Conditioner",
-        price: 15000,
-        photos: "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        checkinTime: new Date('04-01-2021').toLocaleString(),
-        checkoutTime: new Date('05-01-2021').toLocaleString(),
-        rating: 4.1
-      }
-    ]
+    this.roomList = this.roomsService.getRooms();
   }
 
   ngDoCheck(): void {
